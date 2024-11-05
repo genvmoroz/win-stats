a935e27f1003fb2c92c880d40927598c5e033dba
Hennadii Moroz <genvmoroz@gmail.com>
2024-11-05 10:56:06 +0200

    update

--- run-prometheus-collector.bat
+++ run-prometheus-collector.bat
@@ -5,4 +5,4 @@ make all
 cd deployment
 docker-compose rm -f
 docker-compose pull
-docker-compose up --force-recreate --build --abort-on-container-exit
+docker-compose up --force-recreate --build --abort-on-container-exit -d
